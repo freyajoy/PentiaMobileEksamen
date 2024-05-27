cba94678079a719395f34257f7490dcb5bb27d31
alch29 <christensen.alek@gmail.com>
2024-05-27 13:25:55 +0200

    ting

--- projekter.js
+++ projekter.js
@@ -18,15 +18,22 @@ function visProjekter(n) {
     for (i = 0; i < projekter.length; i++) {
         projekter[i].style.display = "none";
     }
-    // for (i = 0; i < projekter.length; i++) {
-    //     projekter[i].classList.remove('vis');
-    //     projekter[i].classList.remove('exit');
-    // }
+
     projekter[projektIndex-1].style.display = "block";
-    // requestAnimationFrame(() => {
-    //     prokter[projekterIndex-1].classList.add('vis');
-    // });
-    // if (n > 1) {
-    //     projekter[projektIndex-2].classList.add('exit');
-    // }
+    
+}
+
+// Tilføj click event listener til hver knap
+let pileKnapper = document.querySelectorAll(".pile");
+
+for (let i = 0; i < pileKnapper.length; i++) {
+    pileKnapper[i].addEventListener("click", function() {
+        if (i === 0) {
+            // Venstre pil
+            plusProjekt(-1);
+        } else {
+            // Højre pil
+            plusProjekt(1);
+        }
+    });
 }
